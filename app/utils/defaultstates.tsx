65a2e112e66516e79a6ed2f3428ffefc51e79fcf
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2024-03-17 06:11:46 +0700

    feat: data dummy

---
+++ app/utils/defaultstates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Bengkulu",
+    country: "ID",
+    state: "Indonesia",
+    lat: -3.788892,
+    lon: 102.266579,
+  },
+  {
+    name: "Palembang",
+    country: "ID",
+    state: "Indonesia",
+    lat: -2.990934,
+    lon: 104.756554,
+  },
+  {
+    name: "Aceh",
+    country: "ID",
+    state: "Indonesia",
+    lat: 4.695135,
+    lon: 96.749397,
+  },
+  {
+    name: "Lampung",
+    country: "ID",
+    state: "Indonesia",
+    lat: -5.45,
+    lon: 105.26667,
+  },
+  {
+    name: "Bogor",
+    country: "ID",
+    state: "Indonesia",
+    lat: -6.595038,
+    lon: 106.816635,
+  },
+];
+
+export default defaultStates;
